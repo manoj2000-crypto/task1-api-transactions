7e3b18c137d986060b2eb54bb73ebfbf84f36523
ManojKaleLaptop <manoj.kale@swatpro.co>
2025-04-12 09:03:24 +0530

    Initialize Project with Basic Structure and Dependencies
    This commit initializes the project with a basic Android application structure and essential dependencies.
    
    - Added necessary project files including `.gitignore`, `build.gradle`, `settings.gradle`, and Gradle wrapper files.
    - Configured the app module with dependencies for:
      - AppCompat
      - Material Design Components
      - Activity
      - ConstraintLayout
      - JUnit (for testing)
      - Retrofit and Gson Converter (for API communication)
      - Biometric Authentication
      - Security Crypto (for encrypted shared preferences)
      - ViewModel and LiveData
      - AndroidX Core KTX
    - Added necessary resource files including layouts, drawables, mipmaps, and string values.
    - Configured the app's `AndroidManifest.xml` with necessary permissions for internet access and biometric authentication, and declared activities.
    - Set up basic themes and colors in the `res/values` directory.

---
+++ app/src/main/java/in/theimperative/task1apitransaction/MainActivity.java
@@ -0,0 +1,29 @@
+package in.theimperative.task1apitransaction;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import in.theimperative.task1apitransaction.ui.login.LoginActivity;
+import in.theimperative.task1apitransaction.utils.TokenManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        String token = TokenManager.getToken(this);
+
+        if (token != null) {
+            // If user is already logged in then goes to verify the biometric.
+            startActivity(new Intent(this, BiometricActivity.class));
+        } else {
+            // If not then logged in and then go to login screen...
+            startActivity(new Intent(this, LoginActivity.class));
+        }
+        finish();
+
+    }
+}
